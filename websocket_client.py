d1b28f0e191982c433f6f7fd3842a928e324c831
Allencs <allenchensheng@163.com>
2020-05-13 12:13:40 +0800

    修复多连接bug，每个连接都需要一个WebSocket对象

--- websocket_client.py
+++ websocket_client.py
@@ -10,48 +10,58 @@ from group_code import group_code
 socket.setdefaulttimeout(20)
 
 
-# 单点：***
-# 群发：***
+# 单点：wss://***/websocket
+# 群发：wss://***/websocket
 
 
 class WebSocketClient(object):
+
+    conns = {}
+
     def __init__(self):
-        self.p2p_ws_address = "***"
-        self.group_ws_address = "***"
-        self.ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE},
-                                      sockopt=socket.setdefaulttimeout(20))
+        self.p2p_ws_address = "wss://***/websocket"
+        self.group_ws_address = "wss://***/websocket"
+        self.own_address = "ws://10.203.29.217:8085/spring_websocket/websocket/"
+        # self.ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE},
+        #                               sockopt=socket.setdefaulttimeout(20))
 
     def p2p_client(self, num):
-        self.ws.connect(self.p2p_ws_address)
-        print(self.p2p_ws_address)
+        ws = websocket.create_connection(self.own_address,
+                                         sslopt={"cert_reqs": ssl.CERT_NONE},
+                                         sockopt=socket.setdefaulttimeout(20))
+        # print(self.p2p_ws_address)
         registry_data = {
             "command": "REGISTER",
             "userInfo": {
                 "accessToken": "user",
-                "channelCode": "***"}
+                "channelCode": "pol4s"}
         }
 
         registry_data['userInfo']['accessToken'] = "user{}".format(num)
 
+        self.conns["user{}".format(num)] = ws
+
         registry_data = json.dumps(registry_data)
 
-        self.ws.send(registry_data)
-
-        self.recv_msg()
+        ws.send(registry_data)
 
     def recv_msg(self):
         while True:
-            msg = self.ws.recv()
-            if msg is not None:
-                # print("Server response: {}".format(msg))
-                print("{} response: {}".format(threading.currentThread().getName(), msg))
+            for conn in self.conns:
+                msg = self.conns[conn].recv()
+                if msg is not None:
+                    print("{} --> response: {}".format(conn, msg))
+                    print("{} response: {}".format(threading.currentThread().getName(), msg))
 
-            else:
-                time.sleep(0.5)
+                else:
+                    time.sleep(0.5)
 
     def group_client(self, num):
-        self.ws.connect(self.group_ws_address)
         print(self.group_ws_address)
+        # self.ws.connect(self.group_ws_address)
+        ws = websocket.create_connection(self.group_ws_address,
+                                         sslopt={"cert_reqs": ssl.CERT_NONE},
+                                         sockopt=socket.setdefaulttimeout(20))
         registry_data = {
             "command": "REGISTER",
             "userInfo": {
@@ -67,15 +77,16 @@ class WebSocketClient(object):
 
         registry_data = json.dumps(registry_data)
 
-        self.ws.send(registry_data)
+        ws.send(registry_data)
 
-        print("Server response: {}".format(self.ws.recv()))
+        print("Server response: {}".format(ws.recv()))
+
+        self.recv_msg()
 
     def multi_p2p_client(self):
-        for i in range(1, 101):
-            td_socket = Thread(target=self.p2p_client, args=(i,),
-                               name="client_{}".format(i))
-            td_socket.start()
+        for i in range(1, 20):
+            self.p2p_client(i)
+        time.sleep(30)
 
     def multi_group_client(self):
         for i in range(1, 101):
@@ -86,8 +97,12 @@ class WebSocketClient(object):
 
 if __name__ == '__main__':
     ws_client = WebSocketClient()
-    # ws_client.p2p_client(1)
-    ws_client.group_client(1)
+    """
+    # 群发：
+    # 点对点：
+    """
+    ws_client.multi_p2p_client()
+    # ws_client.group_client(1)
 
 
 
